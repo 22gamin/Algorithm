1a37255581856d32ea7e345855749f7da3ba6ad9
22gamin <108453365+22gamin@users.noreply.github.com>
2022-07-06 17:16:03 +0900

    Create 1001.java

---
+++ 1001.java
@@ -0,0 +1,16 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class ex1001_subtraction {
+	public static void main(String[] args) {
+		int a=0;
+		int b=0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		a=sc.nextInt();
+		b=sc.nextInt();
+		
+		System.out.println(a-b);
+	}
+}
