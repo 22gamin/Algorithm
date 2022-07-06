7bd7574c4b888928aca7bcbf96e178a3a5f04387
22gamin <108453365+22gamin@users.noreply.github.com>
2022-07-06 17:15:33 +0900

    Create 1000.java

---
+++ 1000.java
@@ -0,0 +1,17 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class ex1000_sum {
+	public static void main(String[] args) {
+		int A=0;
+		int B=0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		A = scanner.nextInt();
+		B = scanner.nextInt();
+		
+
+		System.out.println(A+B);
+	}
+}
