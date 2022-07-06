0e844a324b66d2d22259e47919194e78eed07987
22gamin <108453365+22gamin@users.noreply.github.com>
2022-07-06 17:16:44 +0900

    Create 1008.java

---
+++ 1008.java
@@ -0,0 +1,18 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class ex1008_division {
+	public static void main(String[] args) {
+		double A=0;
+		double B=0;
+		double C=1;
+	
+		Scanner sc = new Scanner(System.in);
+	
+		A=sc.nextDouble();
+		B=sc.nextDouble();
+		
+		System.out.println(A/B);
+	}
+	
+}
