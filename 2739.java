a899e325bec7e936bcbf00cec01389c69abcf207
22gamin <108453365+22gamin@users.noreply.github.com>
2022-07-13 10:44:31 +0900

    Create 2739.java

---
+++ 2739.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex2739_multi_ta {
+	public static void main(String[] args) {
+		int N = 1;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+			
+		for(int i=1; i<10; i++) {
+			System.out.println(N + " * " + i + " = "+ N * i); }
+		 
+	}
+}
