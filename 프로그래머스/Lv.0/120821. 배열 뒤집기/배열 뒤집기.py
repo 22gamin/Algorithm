470c3300e2e9bd550042ba338e41de6dd09bd5de
22gamin <108453365+22gamin@users.noreply.github.com>
2024-01-15 05:37:31 +0900

    Update 배열 뒤집기.py

--- 프로그래머스/Lv.0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/Lv.0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -4,3 +4,7 @@ def solution(num_list):
     answer = num_list
     
     return answer
+
+# answer = num_list.reverse() 의 값이 null이 출력되는데 
+# num_list.reverse()는 원래 num_list값을 변경해줄 뿐 아무런 값이 반환되지 않기 때문에
+# num_list.reverse()는 아무런 값이 없고 num_list를 사용해야 바뀐 배열을 쓸 수 있음
