321918c7c5386871583a3c245d350e778653aec5
22gamin <108453365+22gamin@users.noreply.github.com>
2024-01-17 02:37:19 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,11 @@
+def solution(numbers, direction):
+    
+    if direction == 'right':
+        numbers.insert(0,numbers[-1]) 
+        numbers.pop()
+        
+    if direction == 'left':
+        numbers.append(numbers[0]) 
+        numbers.remove(numbers[0])
+    
+    return numbers
