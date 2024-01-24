08e482b49610a9de5ffadd63031c7c584ce26ecf
22gamin <108453365+22gamin@users.noreply.github.com>
2024-01-24 22:32:00 +0900

    [level 0] Title: OX퀴즈, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,3 +1,19 @@
 def solution(quiz):
     answer = []
+    cnt = 0
+    
+    for i in quiz:
+        a = list(i.split())
+
+        for j in range(len(a)):
+            if a[j] == '+':
+                cnt = int(a[j-1])+int(a[j+1])
+            elif a[j] == '-':
+                cnt = int(a[j-1])-int(a[j+1])
+            elif a[j] == '=':
+                if cnt == int(a[j+1]):
+                    answer.append("O")
+                elif cnt != int(a[j+1]):
+                    answer.append("X")
+        
     return answer
