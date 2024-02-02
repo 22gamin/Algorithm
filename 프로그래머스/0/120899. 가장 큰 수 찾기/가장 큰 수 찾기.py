1ad5352e56916e409fb447da29db66169b6091f9
22gamin <108453365+22gamin@users.noreply.github.com>
2024-02-02 11:33:28 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,6 +1,8 @@
 def solution(array):
     answer = []
-    
+    a = 1234
+    result = list(map(lambda x: x+1, [1,2,3,4,5]))
+    print(result)
     answer.append(max(array))
     answer.append(array.index(max(array)))
     return answer
