124719ecd846d3fc5565ff5b449d59ed501d25e7
22gamin <108453365+22gamin@users.noreply.github.com>
2024-02-05 01:10:28 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,12 @@
+def solution(id_pw, db):
+    answer = 'fail'
+    
+    for i in db:
+        if id_pw == i:
+            return "login"
+        
+        elif id_pw[0] == i[0] and id_pw[1] != i[1]:
+            answer = "wrong pw"
+        
+            
+    return answer
