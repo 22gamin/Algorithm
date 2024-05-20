e7ae488732af4ed65d092aacf994dc0dd178ba30
gamin <108453365+22gamin@users.noreply.github.com>
2024-05-20 20:50:00 +0900

    [Silver IV] Title: 수 찾기, Time: 176 ms, Memory: 51328 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr= list(map(int,input().split()))
+arr_set = set(arr)
+M = int(input())
+
+arr2 = []
+arr2= list(map(int,input().split()))
+
+for j in arr2:
+    if j in arr_set:
+        print(1)
+    else:
+        print(0)
