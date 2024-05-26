c611f5b3304e243ab3dcc6db9b8ddd7761c17783
gamin <108453365+22gamin@users.noreply.github.com>
2024-05-27 00:17:41 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,10 +1,10 @@
 def solution(a, b, n):
     answer = 0
-    cnt = 0
-
-    while(n >= a):
-        cnt = n%a
-        n = ((n//a)*b) 
-        answer += n
-        n += cnt
+    while n >= a:
+        cnt = 0
+        cnt += (n//a)*b
+        answer += (n//a)*b
+        n = cnt+ n%a
+    
+    
     return answer
