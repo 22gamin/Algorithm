55c528c9aaeb7c4c4e5ffa87344ab22080db9c00
gamin <108453365+22gamin@users.noreply.github.com>
2024-05-27 00:15:46 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,12 +1,10 @@
-# a병 주면 b 개를 줌
-# 상빈이는 n개 가지고 있음
 def solution(a, b, n):
     answer = 0
-    while n >= a:
-        cnt = 0
-        cnt += (n//a)*b
-        answer += (n//a)*b
-        n = cnt+ n%a
-    
-    
+    cnt = 0
+
+    while(n >= a):
+        cnt = n%a
+        n = ((n//a)*b) 
+        answer += n
+        n += cnt
     return answer
