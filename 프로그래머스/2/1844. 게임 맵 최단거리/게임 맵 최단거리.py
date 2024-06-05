1affcd67bc36cedd56e80b7c1c5a1166db46333e
gamin <108453365+22gamin@users.noreply.github.com>
2024-06-05 14:19:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.34 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def solution(maps):
+    
+    n = len(maps) # 행
+    m = len(maps[0]) # 열
+    
+    visited = [[0]*(m) for _ in range(n)]
+    
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
+    
+    q = deque([(0,0)])
+    
+    while q:
+        x,y = q.popleft()
+        
+        for i in range(4):
+            ddx = x + dx[i]
+            ddy = y + dy[i]
+            
+            if ddx < 0 or ddx >= n or ddy < 0 or ddy >= m:
+                continue
+                
+            if maps[ddx][ddy]==1 and visited[ddx][ddy] == 0:
+                visited[ddx][ddy] = 1
+                q.append((ddx,ddy))
+                maps[ddx][ddy] = maps[x][y] + 1
+    
+
+    if maps[n-1][m-1] == 1:
+        return -1
+    return maps[n-1][m-1]
+
+
+
