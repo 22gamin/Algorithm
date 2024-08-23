16ca1a484c67aa56dbeabfceb0e75e88407bf7e4
gamin <108453365+22gamin@users.noreply.github.com>
2024-08-23 20:40:34 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,12 @@
+def solution(d, budget):
+    
+    d.sort()
+    cnt = 0
+    
+    for i in d:
+        if i <= budget:
+            budget -= i
+            cnt += 1
+        
+        
+    return cnt
