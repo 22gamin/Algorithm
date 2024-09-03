f7206f723f3a6986fa5b4e458f4d249c7c83b369
gamin <108453365+22gamin@users.noreply.github.com>
2024-09-03 13:30:07 +0900

    [level 2] Title: 스킬트리, Time: 0.10 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -6,7 +6,6 @@ def solution(skill, skill_trees):
         arr = []
         for j in skill:
             arr.append(i.find(j))
-        print(arr)
         
         # -1은 마지막부분만 존재할 수 있음
         # 숫자 차이가 어떻든 순서대로
@@ -14,11 +13,9 @@ def solution(skill, skill_trees):
         if -1 not in arr:
             if arr == arr_:
                 answer += 1
-                print(1)
                 
         else:
             if arr[:len(arr)-arr.count(-1)] == arr_[arr_.count(-1):]:
                 answer += 1
-                print(2)
             
     return answer
