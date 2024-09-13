d46b319991976d0d9035b884ff0a7a7160ee1947
gamin <108453365+22gamin@users.noreply.github.com>
2024-09-13 14:18:46 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.57 ms, Memory: 67.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // 각 원소의 개수 넣음
+        for (int i : array){
+            if (map.containsKey(i)){
+                int value = map.get(i);
+                map.put(i, value+1);
+            }
+            else{
+                map.put(i,1);
+            }
+        }
+        
+        // 그 중 가장 큰 값 -> 최빈값
+        
+        int maxValue = Integer.MIN_VALUE; // 초기값은 최소한으로 설정
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i : map.values()){
+            if (i > maxValue )
+                maxValue = i;
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == maxValue)
+                list.add(entry.getKey());
+        }
+        
+        if (list.size() > 1)
+            return -1;
+        
+    
+  
+    return list.get(0);
+    }
+}
