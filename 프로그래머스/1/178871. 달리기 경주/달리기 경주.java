57c9be6ddf135823a826f9d68cbd15f20b3d6149
gamin <108453365+22gamin@users.noreply.github.com>
2024-09-21 21:01:23 +0900

    [level 1] Title: 달리기 경주, Time: 422.67 ms, Memory: 388 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+        Map<String, Integer> play = new HashMap<>();
+        
+        // hashmap에 값 넣기
+        int cnt = 0;
+        for (int i = 0; i < players.length; i++){
+            play.put(players[i],cnt);
+            cnt++;
+        }
+        
+        // 순위 변동
+        for (int j = 0; j < callings.length; j++){
+            
+            Integer a = play.get(callings[j]); //불린 선수의 원래 순위
+            String beforePlayer = players[a-1];
+            
+            play.put(callings[j], a-1);
+            play.put(beforePlayer, a);
+            
+            players[a-1] = callings[j];
+            players[a] = beforePlayer;
+        }
+        
+        //입력
+        for (String s : play.keySet()){
+            answer[play.get(s)] = s;
+        }
+        
+        return answer;
+    }
+}
