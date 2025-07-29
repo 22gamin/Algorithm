48facf3397c5b4ed17404a36c6a78cacbf970272
gamin <108453365+22gamin@users.noreply.github.com>
2025-07-29 23:42:07 +0900

    [level 2] Title: 올바른 괄호, Time: 16.37 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()){
+            if ( c == ')'){
+                if (stack.isEmpty())
+                    return false;
+               else {
+                   stack.pop();
+               }
+            }
+            else stack.push('(');
+        }
+        
+        
+        if (stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
