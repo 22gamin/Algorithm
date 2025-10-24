319a90e306b16b26fc4642f9b1350fe7b5eb8e82
gamin <108453365+22gamin@users.noreply.github.com>
2025-10-24 17:41:21 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 556 ms, Memory: 48528 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N;
+	
+	public static class dot implements Comparable<dot> {
+		int x;
+		int y;
+		
+		public dot(int x,int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public int compareTo(dot o) {
+			if(this.y == o.y) return this.x - o.x;
+			return this.y - o.y;
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<dot> pq = new PriorityQueue<>();
+		for(int n = 0; n<N; n++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			pq.add(new dot(x,y));
+		}
+		
+		for(int n = 0; n<N; n++) {
+			dot d = pq.poll();
+			sb.append(d.x).append(" ").append(d.y).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
